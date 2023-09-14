a1268fb6ff3bc71b319b45843e6d9458f2271b25
D1abloNope <143985058+D1abloNope@users.noreply.github.com>
2023-09-14 19:11:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите операцию");
+char operation = Convert.ToChar(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int results = (0);
+if (operation == '+')
+{
+    results += num1 + num2;
+    Console.WriteLine(num1 + "+" + num2 + "=" + results);
+}
+
+else if (operation == '-')
+{
+    results += num1 - num2;
+    Console.WriteLine(num1 + "-" + num2 + "=" + results);
+}
+
+else if (operation == '*')
+{
+    results += num1 * num2;
+    Console.WriteLine(num1 + "*" + num2 + "=" + results);
+}
+
+else if (operation == '/')
+{
+    if (num2 == 0)
+    {
+        Console.WriteLine("Нельзя делить на 0");
+    }
+    else
+    {
+        results += num1 / num2;
+        Console.WriteLine(num1 + "/" + num2 + "=" + results);
+    }
+}
+else
+{
+    Console.WriteLine("Неизвестный оператор");
+}
