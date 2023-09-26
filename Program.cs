6305cacc8136bd7757584afda6c5726775807d12
Anastasia <girl-physicist@mail.ru>
2023-09-26 16:00:24 +0300

    comments

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
 ﻿Console.WriteLine("Введите первое число");
-int num1 = Convert.ToInt32(Console.ReadLine());
+int num1 = Convert.ToInt32(Console.ReadLine()); // следовало добавить обработку исключения на случай, если не удалось конвертировать строку в число (например ввели дробное число или буквы)
 Console.WriteLine("Введите операцию");
-char operation = Convert.ToChar(Console.ReadLine());
+char operation = Convert.ToChar(Console.ReadLine()); // следовало добавить обработку исключения на случай, если не удалось конвертировать строку в символ (например ввели больше 1 символа)
 Console.WriteLine("Введите второе число");
-int num2 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine()); // следовало добавить обработку исключения на случай, если не удалось конвертировать строку в число (например ввели дробное число или буквы)
+// перебор операций удобнее делать через конструкцию switch case
 int results = (0);
 if (operation == '+')
 {
